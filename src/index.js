4d7a64346b4106a514a61690a544b9a439c2fa7e
Swad <SwadicalRag@users.noreply.github.com>
2016-03-10 10:57:53 +1030

    JS stuff

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 var $ = require("jquery-browserify");
 var flinders = require("flinders-api");
 
-console.log(flinders.bus.getBusLocation());
+function updateElements() {
+    var data = flinders.bus.getBusLocation();
+
+    $("#bus-location").text(data.position);
+    $("#progress-bar-parent").attr("data-tooltip",data.timeLeft + " seconds left");
+    $("#progress-bar-child").css("width",(data.percentage * 100) + "%");
+    $("#bus-next-stop").text(data.nextStop);
+}
+
+$(document).ready(function() {
+    updateElements();
+    setInterval(updateElements,100);
+});
