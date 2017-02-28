f4bd5a8ce6dcd1ea2020035ef89a77740a57959a
Swad <SwadicalRag@users.noreply.github.com>
2017-02-28 18:30:21 +1030

    Ditch dynamic updating and manually update 2017 lecture list

--- js/src/lib/topics.ts
+++ js/src/lib/topics.ts
@@ -11,20 +11,20 @@ export function getTopics() {
 }
 
 export function fetchTopics(year:number) {
-    getPromise(`http://api.unibuddy.com.au/api/v2/uni/flinders/topics.json?&inst_code=flinders&year=${year}&semester=NS1,S1`)
-        .then((resp) => {
-            if((resp.code == 200) || (resp.code == 304)) {
-                let topics = JSON.parse(resp.contents);
+    // getPromise(`http://api.unibuddy.com.au/api/v2/uni/flinders/topics.json?&inst_code=flinders&year=${year}&semester=NS1,S1`)
+    //     .then((resp) => {
+    //         if((resp.code == 200) || (resp.code == 304)) {
+    //             let topics = JSON.parse(resp.contents);
 
-                _topicListInternal = topics.data;
-            }
-            else {
-                throw new Error(`http code ${resp.code} != 200!`);
-            }
-        })
-        .catch(() => {
-            console.log(`Couldn't update topic list for ${year}`);
-        })
+    //             _topicListInternal = topics.data;
+    //         }
+    //         else {
+    //             throw new Error(`http code ${resp.code} != 200!`);
+    //         }
+    //     })
+    //     .catch(() => {
+    //         console.log(`Couldn't update topic list for ${year}`);
+    //     })
 }
 
 fetchTopics(getYear());
